32e0e2b71afe7dd73e4e58ed603bfa0cbd6db287
Andy Dobbs <andrew.dobbs@gmail.com>
2024-01-05 05:33:26 -0500

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Andy Dobbs on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
